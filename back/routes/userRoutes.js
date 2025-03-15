8d9448225fbb61922996042d62a329bcf0aae1d6
Yeshwanth-Vardhan <naniyeshwanth37@gmail.com>
2025-03-15 15:56:20 +0530

    Initial commit

---
+++ back/routes/userRoutes.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+router.get("/:userId/leaveBalance", async (req, res) => {
+  const user = await User.findById(req.params.userId);
+  if (!user) return res.status(404).json({ message: "User not found" });
+
+  res.json({
+    casualLeaveBalance: user.casualLeaveBalance,
+    medicalLeaveBalance: user.medicalLeaveBalance,
+  });
+});
+
+module.exports = router;
